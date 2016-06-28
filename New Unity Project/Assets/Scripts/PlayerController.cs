0a29041f7c3e1f3b55b8c8810bf6db593d4a0790
halfoff <psychotuna@gmail.com>
2016-06-28 18:51:32 -0500

    Progress + Organization

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveH = Input.GetAxis ("Vertical");
+		Vector3 mov = new Vector3 (0, 0, moveH);
+		rb.AddForce (mov * speed);
+	}
+
+}
